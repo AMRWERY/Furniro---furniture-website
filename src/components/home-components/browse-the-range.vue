e6cc827228b590017a1ca3085bd2b69ebd8ea84d
AMRWERY <amrmounir2@gmail.com>
2025-03-08 22:43:45 +0200

    .

--- src/components/home-components/browse-the-range.vue
+++ src/components/home-components/browse-the-range.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <div class="px-4 mt-[56.74px] mb-[56px] mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="text-center">
-                <p class="font-bold text-[20px] sm:text-[24px] md:text-[32px] text-[#333333]">Browse The Range</p>
-                <span class="text-[#666666] text-sm sm:text-base md:text-xl font-normal">Lorem ipsum dolor sit amet,
+                <p class="font-700 font-poppins text-[20px] sm:text-[24px] md:text-[32px] text-[#333333]">Browse The
+                    Range</p>
+                <span class="text-[#666666] text-sm sm:text-base md:text-xl font-400 font-poppins">Lorem ipsum dolor sit
+                    amet,
                     consectetur adipiscing elit.</span>
 
-                <div class="grid grid-cols-3 gap-x-6 gap-y-10 xl:gap-x-8 mt-12">
-                    <div v-for="card in cards" :key="card.id" class="group relative">
+                <div
+                    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mt-[30px] sm:mt-[44px] md:mt-[62.29px] gap-x-6 gap-y-10 xl:gap-x-8">
+                    <div v-for="card in cards" :key="card.id" class="relative group">
                         <img :src="card.imageSrc"
-                            class="aspect-square rounded-md bg-gray-200 object-cover group-hover:opacity-75 md:w-[381px] md:h-[480px] mx-auto sm:w-[300px] sm:h-[400px] w-[150px] h-[200px]" />
+                            class="aspect-square rounded-md bg-gray-200 object-cover group-hover:opacity-75 md:w-[381px] md:h-[480px] mx-auto w-[300px] h-[400px] max-w-max" />
                         <div class="flex justify-center">
                             <div>
                                 <h3
-                                    class="md:text-2xl font-semibold text-[#333333] text-center text-base sm:text-lg mt-1 sm:mt-2 md:my-4">
+                                    class="md:text-2xl font-poppins font-600 text-[#333333] text-center text-base sm:text-lg mt-1 sm:mt-2 md:mt-[30px]">
                                     <router-link to="">
                                         <span aria-hidden="true" class="absolute inset-0" />
                                         {{ card.name }}
