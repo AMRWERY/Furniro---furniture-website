d47972f86f15f3dd0ba52ab52a95438e50cb4763
AMRWERY <amrmounir2@gmail.com>
2025-03-08 22:22:59 +0200

    .

--- src/components/home-components/hero-banner.vue
+++ src/components/home-components/hero-banner.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="pb-6 bg-white sm:pb-8 lg:pb-12">
             <section
-                class="relative flex items-center justify-center flex-1 py-16 overflow-hidden bg-gray-100 shadow-lg md:justify-end min-h-96 shrink-0 md:py-20 xl:py-48">
+                class="relative flex items-center justify-center flex-1 py-16 overflow-hidden bg-gray-100 md:justify-end min-h-96 shrink-0 md:py-20 xl:py-48">
                 <img src="https://justfields.com/storage/projects/7M5rV059/hero-section-banner.png" loading="lazy"
                     alt="hero-section-banner" class="absolute inset-0 object-cover object-center w-full h-full" />
 
@@ -14,11 +14,10 @@
                                 New Arrival
                             </h1>
                             <p
-                                class="mb-4 text-lg text-[#B88E2F] sm:text-xl md:text-[52px] font-700 font-poppins leading-[65px] text-center md:text-start">
+                                class="mb-4 text-lg text-[#B88E2F] sm:text-xl sm:leading-[65px] md:text-[52px] font-700 font-poppins leading-[65px] text-start">
                                 Discover Our New Collection
                             </p>
-                            <span
-                                class="mb-6 text-sm font-medium text-center sm:text-base md:text-lg md:text-start">Lorem
+                            <span class="mb-6 text-sm font-medium sm:text-base md:text-lg text-start">Lorem
                                 ipsum dolor sit amet,
                                 consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</span>
                             <router-link to=""
